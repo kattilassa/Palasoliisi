adc48ef1daa97ecce059b3ee65863cc60cdf2cf2
Kati Savolainen <kati.savolainen@tuni.fi>
2025-04-04 14:39:10 +0300

    major bug fixes

--- Code/PopUp.cs
+++ Code/PopUp.cs
@@ -9,16 +9,21 @@ namespace PalaSoliisi
 
 		public override void _Ready()
 		{
+			Level.Current._isDialogueRunning = true;
 			_okButton.Connect(Button.SignalName.Pressed,
 				new Callable(this, nameof(OnOkButtonPressed)));
+
 		}
 
 		private void OnOkButtonPressed()
 		{
 			// Pois / piilota ??
+
 			this.Hide();
 			this.QueueFree();
+			Level.Current._isDialogueRunning = false;
 			Level.Current.Dialogue();
+
 		}
 	}
 }
